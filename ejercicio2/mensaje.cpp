39b3359fdbee19267e683323b3d7ee76547fc14f
Henry La Chira <henrylachira1995@gmail.com>
2021-02-08 17:07:50 -0600

    añaniedo ejercicio2

---
+++ ejercicio2/mensaje.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string nombre;
+
+    cout<<"Ingrese su nombre: ";
+    cin>>nombre;
+
+    cout<<endl;
+    cout<<"Hola mi nombre es "<<nombre;
+    return 0;
+}
