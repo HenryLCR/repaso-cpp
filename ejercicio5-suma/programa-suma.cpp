fe76a94801fbc78d65d57426c34ffb93e25bbab4
Henry La Chira <henrylachira1995@gmail.com>
2021-02-08 18:31:05 -0600

    agregando ejercicio 5 programa suma

---
+++ ejercicio5-suma/programa-suma.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=10, b=12, resultado;
+    
+    resultado = a+b;
+
+    cout<<"La suma de a + b es: "<< resultado <<endl;
+
+    return 0;
+}
